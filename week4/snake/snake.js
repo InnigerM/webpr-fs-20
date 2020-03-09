cd4b76c0efebc582fcf022f802102a69202e3bd4
InnigerM <marco.inniger@gmail.com>
2020-03-09 11:03:46 +0100

    Added course materials for week 4

--- week4/snake/snake.js
+++ week4/snake/snake.js
@@ -1,10 +1,10 @@
 // requires ../lambda/lambda.js
 
 
-const north = Pair( 0)(-1);
-const east  = Pair( 1)( 0);
-const south = Pair( 0)( 1);
-const west  = Pair(-1)( 0);
+const north = Pair(0)(-1);
+const east = Pair(1)(0);
+const south = Pair(0)(1);
+const west = Pair(-1)(0);
 
 let direction = north;
 
@@ -20,13 +20,13 @@ let snake = [
 let food = Pair(15)(15);
 
 // function snakeEquals(a, b) { return a.x === b.x && a.y === b.y }
-const pairEq = a => b =>  undefined; // todo: your code here
+const pairEq = a => b => a(fst) === b(fst) && a(snd) === b(snd);
 
 // Pair + Pair = Pair        // Monoid
-const pairPlus = a => b =>  undefined; // todo: your code here
+const pairPlus = a => b => Pair(a(fst) + b(fst))(a(snd) + b(snd));
 
 // Function and Pair = Pair  // Functor
-const pairMap = f => p =>  undefined; // todo: your code here
+const pairMap = f => p => Pair(f(p(fst)))(f(p(snd)));
 
 
 function changeDirection(orientation) {
@@ -37,11 +37,13 @@ function changeDirection(orientation) {
 /**
  * when trying to get an element by id from the dom, the element might not be there
  * but in this case the application should not crash randomly
-* @return Either ErrorMessage or HTMLElement
-*/
+ * @return Either ErrorMessage or HTMLElement
+ */
 function safeGetElementById(id) {
     let result = document.getElementById(id);
-    return result === undefined; // todo: your code here
+    return result === undefined
+        ? Left("element with id " + id + "not found")
+        : Right(result);
 }
 
 const log = s => console.log(s);
@@ -49,6 +51,9 @@ const log = s => console.log(s);
 function start() {
 
     // todo: if safeGetElementById("canvas") yields an error message, log it. Otherwise startWithCanvas
+    either(safeGetElementById("canvas"))
+    (console.log()) // Error Case
+        (startWithCanvas()) //OK Case
 
 }
 
@@ -57,7 +62,7 @@ const startWithCanvas = canvas => {
     const context = canvas.getContext("2d");
 
     const rightArrow = 39;
-    const leftArrow  = 37;
+    const leftArrow = 37;
     window.onkeydown = evt => {
         const orientation = (evt.keyCode === rightArrow) ? clockwise : countercw;
         changeDirection(orientation);
@@ -70,8 +75,12 @@ const startWithCanvas = canvas => {
 };
 
 const inBounds = max => x => {
-    if (x < 0)   { return max - 1 }
-    if (x > max) { return 0 }
+    if (x < 0) {
+        return max - 1
+    }
+    if (x > max) {
+        return 0
+    }
     return x
 };
 
@@ -80,7 +89,7 @@ function nextBoard() {
     const oldHead = snake[0];
 
     const newHead = undefined; // todo: your code here: old head plus direction
-    const head    = undefined; // todo: your code here: new head put in bounds
+    const head = undefined; // todo: your code here: new head put in bounds
 
     const pickRandom = () => Math.floor(Math.random() * max);
     if (true) {  // todo: have we found any food?
